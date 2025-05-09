067602b297cd6634ed5dce384a16d0d0d3e1661c
Martina Best <martinabest@Martinas-MacBook-Pro.local>
2025-05-09 16:28:20 +1200

    last stage

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 import { useContext } from "react";
 import Image from "next/image";
 import { CursorContext } from "@/components/CursorContext";
-import Form from "@/components/Form";
 
 const Contact = () => {
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -28,11 +28,10 @@ const Contact = () => {
             className="flex-1 flex flex-col justify-center"
           >
             <h3 className="h3 mb-8 text-center xl:text-left">Contact info</h3>
-            {/* items */}
+
             <div className="flex flex-col items-center xl:items-start gap-12">
-              {/* item */}
+              {/* Location */}
               <div className="flex items-start gap-4">
-                {/* icon */}
                 <div className="relative w-[36px] h-[36px]">
                   <Image src="/assets/contact/pin.svg" fill alt="" />
                 </div>
@@ -45,54 +44,28 @@ const Contact = () => {
                   </p>
                 </div>
               </div>
-              {/* end item */}
-              {/* item */}
+
+              {/* Phone */}
               <div className="flex items-start gap-4">
-                {/* icon */}
                 <div className="relative w-[36px] h-[36px]">
                   <Image src="/assets/contact/phone.svg" fill alt="" />
                 </div>
                 <div className="pt-1 flex-1">
                   <h4 className="h4 mb-2">Phone number</h4>
-                  <div className="pt-1 flex-1">
-                    <p>Phone: +60(0) 163 401 229</p>
-                    <p></p>
-                  </div>
+                  <p>Phone: +60(0) 163 401 229</p>
                 </div>
               </div>
-              {/* end item */}
-              {/* item */}
+
+              {/* Email */}
               <div className="flex items-start gap-4">
-                {/* icon */}
                 <div className="relative w-[36px] h-[36px]">
                   <Image src="/assets/contact/email.svg" fill alt="" />
                 </div>
                 <div className="pt-1 flex-1">
                   <h4 className="h4 mb-2">Email address</h4>
-                  <div className="flex flex-col gap-1">
-                    <p>admin@samanthatheclair.com</p>
-                    <p></p>
-                  </div>
+                  <p>admin@samanthatheclair.com</p>
                 </div>
               </div>
-              {/* end item */}
-            </div>
-          </motion.div>
-          {/* form */}
-          <motion.div
-            onMouseEnter={mouseEnterHandler}
-            onMouseLeave={mouseLeaveHandler}
-            initial={{ opacity: 0, x: 60 }}
-            animate={{
-              opacity: 1,
-              x: 0,
-              transition: { delay: 2.4, duration: 0.8, ease: "easeInOut" },
-            }}
-            className="flex-1"
-          >
-            <div className="bg-[#f0cfbc] w-full max-w-[580px] gap-4 p-10 mx-auto xl:mx-0">
-              <h3 className="h3 mb-8 text-center">Get in touch</h3>
-              <Form />
             </div>
           </motion.div>
         </div>
